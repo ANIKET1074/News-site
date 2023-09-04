2aeb6e77b14ff2070cafb8f4fe913334f13e9d7a
JADAV ANIKET KAMLESHKUMAR <86431201+ANIKET1074@users.noreply.github.com>
2023-09-04 22:30:31 +0530

    Footer file added

---
+++ footer.php
@@ -0,0 +1,23 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                $sql = "SELECT * FROM `settings`";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                        <span><?php echo $row['footer_desc'] ?></span>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
